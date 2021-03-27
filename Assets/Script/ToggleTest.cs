db4ae9cf32b2233837ec9bea6f0a49a55309abb2
SyPoP <suyao100@qq.com>
2021-03-27 15:47:11 +0800

    test

---
+++ Assets/Script/ToggleTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.gameObject.GetComponent<Toggle>().onValueChanged.AddListener((bool change) => ChangeValue(change));
+            }
+
+    private void ChangeValue(bool change)
+    {
+        Debug.Log("toggle changed");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/subscribeBtton.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class subscribeBtton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.gameObject.GetComponent<Button>().onClick.AddListener(OnButtonClick);
+        String str = "str";
+        //委托按钮
+        this.gameObject.GetComponent<Button>().onClick.AddListener(() => OnButtonClickParameter(str));
+    }
+
+    private void OnButtonClick()
+    {
+        Debug.Log("Click");
+    }
+
+    private void OnButtonClickParameter(string str)
+    {
+        Debug.Log(str);
+
+    }
+}
